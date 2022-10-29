e06db70a136cc86b07e73a57081e67d7dea8c777
usha-12 <usha12ahirwar@gmail.com>
2022-10-29 13:02:59 +0530

    debit withdraw from account

--- src/com/brideglabz/stockaccountmanagement/AccountManager.java
+++ src/com/brideglabz/stockaccountmanagement/AccountManager.java
@@ -1,2 +1,17 @@
-package com.brideglabz.stockaccountmanagement;public class AccountManager {
+package com.brideglabz.stockaccountmanagement;
+public class AccountManager {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Stock Account Management Program!");
+
+        StockPortfolio portfolio = new StockPortfolio();
+        portfolio.selectActivity();
+    }
 }
+/*Write a program to read in Stock Names, Number of Share, Share Price.
+Print a Stock Report with the total value of each Stock and
+the total value of Stock.
+Modify class Account to provide a method called debit that withdraws money
+from an Account. Ensure that the debit amount does not exceed the
+Account’s balance. If it does, the balance should be left unchanged and the
+method should print a message indicating ―Debit amount exceeded account balance.
+Modify class AccountTest to test method debit.*/
